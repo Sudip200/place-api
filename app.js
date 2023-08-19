736e5452a5b92034085e57cbb58e3321ac6d9667
Sudipto Das <98449874+Sudip200@users.noreply.github.com>
2023-08-19 07:42:15 +0000

    adding student post

--- app.js
+++ app.js
@@ -731,13 +731,28 @@ app.post('/stlogin',async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 })
-app.post('/createpost',upload.single('file'),async (req,res)=>{
+app.post('/createpost',uploads.single('file'),async (req,res)=>{
   const {id,content}=req.body;
   const file=req.file;
   try{
-     const post =new Post({content:content,user:id,photo:file});
+    const photoParams = {
+      Bucket: "uploadfiles200",
+      Key: `${Date.now()}_${req.file.originalname}`,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype, // Set Content-Type based on file type
+    };
+    const logoUploadResult = await s3.upload(photoParams).promise();
+    const post =new Post({content:content,user:id,photo:logoUploadResult.Location});
     await  post.save();
     res.json('uploaded');
+  }catch(error){
+    res.json(error);
+  }
+})
+app.get('/allposts',async (req,res)=>{
+  try{
+  const posts= await Post.find({}).populate('user');
+    res.json(posts);
   }catch(error){
     res.json(error);
   }
